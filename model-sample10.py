61a205b64e489ef4d7669de5b5edac4374dfd3c1
podhmo <podhmo+altair@beproud.jp>
2016-03-19 21:09:00 +0900

    ordering by joined counted value

---
+++ model-sample10.py
@@ -0,0 +1,76 @@
+# -*- coding:utf-8 -*-
+
+"""
+ordering by joined count
+"""
+
+# -*- coding:utf-8 -*-
+import django
+from django.db import models
+from django.conf import settings
+from django.db import connections
+from django.core.management.color import no_style
+
+
+settings.configure(
+    DEBUG=True,
+    DATABASES={"default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:"
+    }},
+    INSTALLED_APPS=[__name__]
+)
+
+
+def create_table(model):
+    connection = connections['default']
+    cursor = connection.cursor()
+    sql, references = connection.creation.sql_create_model(model, no_style())
+    for statement in sql:
+        cursor.execute(statement)
+
+    for f in model._meta.many_to_many:
+        create_table(f.rel.through)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=32, default="foo", blank=False)
+
+    class Meta:
+        app_label = __name__
+
+
+class Comment(models.Model):
+    subject = models.ForeignKey(Subject, null=False)
+    content = models.TextField(default="")
+
+    class Meta:
+        app_label = __name__
+
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    django.setup()
+
+    create_table(Subject)
+    create_table(Comment)
+
+    subject = Subject(name="X")
+    subject.save()
+    Comment(subject=subject, content="x").save()
+    Comment(subject=subject, content="xx").save()
+
+    subject = Subject(name="Y")
+    subject.save()
+    Comment(subject=subject, content="y").save()
+
+    subject = Subject(name="Z")
+    subject.save()
+    Comment(subject=subject, content="z").save()
+    Comment(subject=subject, content="zz").save()
+    Comment(subject=subject, content="zzz").save()
+
+    qs = Subject.objects.annotate(c=models.Count('comment__subject_id')).order_by("-c")
+    for subject in qs:
+        print(subject.name, subject.c)
